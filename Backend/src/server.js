1280a13d52c5d8437c2cbd3a4b656dae777a6e1c
Hamza <hamzafareed616@gmail.com>
2025-12-13 15:05:10 +0500

    ready for production

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import path from "path"
 
 
 dotenv.config();
@@ -10,6 +11,19 @@ const app = express();
 
 app.use("/api/auth",authRoutes);
 app.use("/api/message",messageRoutes);
+
+const __dirname= path.resolve();
+
+
+// For Deployment 
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname,"../Frontend/dist")))
+
+  app.get(/^\/.*$/,(req,res)=>{
+    res.sendFile(path.join(__dirname,"../Frontend/dist/index.html"))
+  })
+}
 
 
 app.listen(process.env.PORT,()=>{
